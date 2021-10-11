bd4db0e8d1958c5f1cd652206f37bdb3027175f2
Eoan O Dea <odeaeoan@gmail.com>
2021-10-11 17:57:45 +0200

    Added readme & example.env

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,33 +2,13 @@
  * Primary dependencies
  */
 import { Request, Response, NextFunction } from "express";
-// import fetch from "../mod.cjs";
-// import https from "https";
-import axios from "axios";
-// const fetch = await import("node-fetch");
-
-/**
- * Model Schema
- */
-// import User from "../models/user.model";
-
-/**
- * JSON Web token imports
- */
-import jwt from "jsonwebtoken";
-import expressJwt from "express-jwt";
-
-/**
- * Importing environment variables
- */
-import config from "./../../config/config";
 
 /**
  * Helpers for success / error responses
  */
 import { handleError, handleSuccess } from "../helpers/responseHandler";
 import RequestMiddleware from "../interfaces/express";
-import { IAccessToken, IPerson } from "..//interfaces/plutio";
+import { fetchPlutioUser, generateAccessToken } from "./plutio.controller";
 
 /**
  * Handle a user signin
@@ -39,20 +19,6 @@ import { IAccessToken, IPerson } from "..//interfaces/plutio";
 export const signin = async (req: Request, res: Response) => {
   try {
     const { email, password } = req.body;
-
-    /**
-     * Find a user with this email
-     */
-    // const user: any = await User.findOne({ email });
-
-    /**
-     * If no user is found, or if the password
-     * doesn't match, drrthrow an error
-     */
-    // if (!user) throw new Error(`No user exists with the email ${email}`);
-
-    // if (!user.authenticate(password))
-    //   throw new Error("Email and Password don't match");
 
     const plutioToken: any = await generateAccessToken();
     console.log(plutioToken);
@@ -72,30 +38,10 @@ export const signin = async (req: Request, res: Response) => {
     if (password !== pin) throw new Error("Pin does not match");
 
     /**
-     * Sign the user's unique ID into a
-     * JSON Web Token string payload
-     */
-    // const token = jwt.sign(
-    //   {
-    //     _id: user._id,
-    //   },
-    //   config.jwtSecret
-    // );
-
-    /**
-     * Set the token as a cookie in the response
-     */
-    // res.cookie("t", token, {
-    //   expires: new Date(Date.now() + parseInt(config.SESSION_TTL, 10)),
-    //   httpOnly: false,
-    // });
-
-    /**
      * Return a 200 response with the token and user
      */
     return res.status(200).json(
       handleSuccess({
-        // token,
         user: {
           name: user.name.first + " " + user.name.last,
           email: user.contactEmails[0].address,
@@ -123,14 +69,6 @@ export const signout = async (req: Request, res: Response) => {
 };
 
 /**
- * Ensure a user is signed in before continuing
- */
-// export const requireSignin = expressJwt({
-//   secret: config.jwtSecret,
-//   userProperty: "auth",
-// });
-
-/**
  * Ensure a user has authorization, and is the logged in user before continuing
  * If not, respond with a 403 response
  */
@@ -149,80 +87,4 @@ export const hasAuthorization = (
   }
 
   next();
-};
-
-// /**
-//  * Get a user by their access token
-//  *
-//  * @param {Request} req
-//  * @param {Response} res
-//  */
-// export const getUser = async (req: RequestMiddleware, res: Response) => {
-//   /**
-//    * Find a user with this email
-//    */
-//   const user: any = await User.findById(req.auth._id);
-
-//   /**
-//    * Return a 200 response with the token and user
-//    */
-//   return res.status(200).json(
-//     handleSuccess({
-//       token: req.params.token,
-//       user: { name: user.name, email: user.email, _id: user._id },
-//     })
-//   );
-// };
-
-const base = "https://api.plutio.com";
-
-/**
- * Fetch a person by ID
- *
- * @param {token: String}
- * @param {id: String}
- */
-export const fetchPlutioUser = async (token: string, email: string) => {
-  try {
-    const response = await axios.get(
-      `${base}/v1.8/people?role=team&contactEmails.address=${email}`,
-      {
-        method: "GET",
-        // @ts-ignore
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-          Business: process.env.PLUTIO_BUSINESS_DOMAIN,
-        },
-      }
-    );
-
-    return response.data;
-
-    // return response.json();
-  } catch (err) {
-    return console.log(err);
-  }
-};
-
-/**
- * Generates a Plutio Access Token
- *
- * @returns
- */
-const generateAccessToken = async () => {
-  try {
-    const urlencoded = new URLSearchParams();
-
-    urlencoded.append("client_id", process.env.PLUTIO_CLIENT_ID || "");
-    urlencoded.append("client_secret", process.env.PLUTIO_CLIENT_SECRET || "");
-    urlencoded.append("grant_type", "client_credentials");
-
-    const data = await axios.post(`${base}/v1.8/oauth/token`, urlencoded);
-    return data.data;
-  } catch (err) {
-    console.log("Error!", err);
-    return err;
-  }
 };
